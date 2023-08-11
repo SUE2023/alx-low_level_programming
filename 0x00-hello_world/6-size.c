dc38ed5b488ab593d993e122ae5d0911073b385b
Susan <susansewe@gmail.com>
2023-08-10 18:20:54 -0700

    Hello World project tasks 0-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("%c,", sizeof(char);
+  printf("%i,", sizeof(int);
+  printf("%li,", sizeof(long int);
+  printf("%lli,", sizeof(long long int);
+  printf("%f,", sizeof(f);
+  return (0);
+}
