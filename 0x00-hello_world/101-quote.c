f3ee2cf30295d41d6d5875914f41b9ec77133b1f
Susan <susansewe@gmail.com>
2023-08-16 00:57:54 -0700

    Correction to Hellow World and Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h)
+#include<unistd.h)
+/**
+ *main - Program that print to start error
+ *return = 1
+ */
+int main(void)
+{
+  fput("and that piece of art is useful\"\-Dora Korpar\,2015-10-19\n", stdou);
+  return (1);
+}
