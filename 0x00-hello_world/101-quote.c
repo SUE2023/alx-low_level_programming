6ba28d59c2424629bd74a67a77a77b50922a2f83
Susan <susansewe@gmail.com>
2023-08-16 01:31:44 -0700

    Correction Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h)
-#include<unistd.h)
+#include<stdio.h>
+#include<unistd.h>
 /**
  *main - Program that print to start error
  *return = 1
  */
 int main(void)
 {
-  fput("and that piece of art is useful\"\-Dora Korpar\,2015-10-19\n", stdou);
+  fput("and that piece of art is useful\"\-Dora Korpar\,2015-10-19\n", stdout);
   return (1);
 }
