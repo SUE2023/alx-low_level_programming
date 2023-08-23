18dd0da89c6bd04e0e68d6a6ef8d13ba5993be70
Susan <susansewe@gmail.com>
2023-08-23 08:19:20 -0700

    4-print_alphabt.c FINALLY

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-  char alphabet, q, e;
-  q = 'q';
-  e = 'e';
+	char alphabet, q, e;
 
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      if (alphabet != e && != q)
-      putchar(alphabet);
-    }
-  putchar('\n');
-
-  return (0);
+	q = 'q';
+	e = 'e';
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != e && alphabet != q)
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
