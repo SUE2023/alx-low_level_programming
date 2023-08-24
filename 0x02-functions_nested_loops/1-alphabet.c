8bf80814ed5df7ad4cc97e1661b316b43283317c
Susan <susansewe@gmail.com>
2023-08-24 11:09:30 -0700

    task 0x02, 0x05, 6-cap_string.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main – checking the code
+* return (0) success
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts - a function that prints a string, followed by a new line, to stdout.
+*Description: printing  a string to stout
+*@*str: a string to be printed
+*/
+void _puts(char *str)
+{
+	while (*str != "\0")
+	{
+		_putchar(*str++);
+	}
+	_putchar("\n");
+}
+
