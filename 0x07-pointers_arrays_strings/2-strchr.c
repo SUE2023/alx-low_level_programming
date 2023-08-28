a3937b5efed440633fe98d75ee02b047dc67752b
Susan <susansewe@gmail.com>
2023-08-28 05:16:26 -0700

    Tasks 0-1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * Description: locates a character in a string
+ * @c: character to be allocated
+ * @s: string with the character
+ * Return:(NULL)
+ */
+char *_strchr(char *s, char c)
+{
+	
+}
