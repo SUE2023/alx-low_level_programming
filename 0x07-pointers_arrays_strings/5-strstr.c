c9fc9af6538a5f653f6bb334a8fbf4c6ec19807b
Susan <susansewe@gmail.com>
2023-08-28 09:26:18 -0700

    Task 2-5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stddef.h>
+/**
+ * _strstr -  locates a substring
+ * Description: first occurance of substring in a string
+ * @needle: substring
+ * @haystack: string
+ * Return: (NULL)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *one = haystack;
+	char *two = needle;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<stddef.h>
+/**
+ * print_chessboard - prints the chessboard
+ * Description: prints chessboard
+ * @a
+ * @8:
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{}
