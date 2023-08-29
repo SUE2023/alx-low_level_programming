8a97212fe16e8b4d33023a4debbaefa7efd52b37
Susan <susansewe@gmail.com>
2023-08-29 01:48:16 -0700

    Tasks 5-9

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,9 +3,19 @@
 /**
  * print_chessboard - prints the chessboard
  * Description: prints chessboard
- * @a
- * @8:
+ * @a- array
  * Return: void
  */
 void print_chessboard(char (*a)[8])
-{}
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
