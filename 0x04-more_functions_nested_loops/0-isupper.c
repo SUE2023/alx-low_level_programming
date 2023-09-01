0b5a40e8c53ca16387a14e2ad9a501ba51142f6d
Susan <susansewe@gmail.com>
2023-09-01 14:20:37 -0700

    correction 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,10 @@
  * _isupper - checks for uppercase character
  * Description: checks for uppercase character
  * @c: input character
- * return: (1)
+ * Return: (1)
  */
 int _isupper(int c)
-{	
+{
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
