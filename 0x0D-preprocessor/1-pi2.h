3e5d50a01df882cdc7db5dd5a3e58d4a9a7e78c5
Susan <susansewe@gmail.com>
2023-09-12 11:01:12 -0700

    Task's Corrections 2, 4

---
+++ 0x0D-preprocessor/1-pi2.h
@@ -0,0 +1,9 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+/**
+ * Fiele: 1-pi.h
+ * Dscription: header file
+ */
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro2.h
@@ -0,0 +1,13 @@
+#ifndef _MACROLIKE_H_
+#define _MACROLIKE_H_
+/**
+ * File: 3-function_like_macro
+ * @x: input value (an int)
+ * Description: header file that compute absolulte value
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x): (x))
+
+/* : act as else, ? means multiply by -1 or number to neutralize the negative */
+
+#endif
