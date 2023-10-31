2b8ce18c1d60155c4eb98873a99eea91a481038b
Susan <susansewe@gmail.com>
2023-10-31 11:29:01 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,28 +10,24 @@
 char *_strdup(char *str)
 {
 	char *ptstr;
-	char strlength = 0;
+	int strlength = 0;
 	int count;
 
+	if (str == NULL)
+		return (NULL);
 	/* to know length of the string */
-	while (strlength != '\0')
-	{
-		strlength++;
-	}
+	count = 0;
+	while (str[count] != '\0')
+		count++;
 	/* size in bytes to be allocated */
-	ptstr =	(char *)malloc(strlength * sizeof(char) + 1);
+	ptstr =	(char *)malloc(sizeof(char) * (count + 1));
 	/*check if malloc does not return null */
 	if (ptstr == NULL)
-	{
 		return (NULL);
-	}
 	/* copying this string to the new memory location */
-
-	for (count = 0; str[count] != '\0'; count++)
-	{
-		ptstr[count] = str[count];
-	}
-	ptstr[count] = '\0';
+	for (strlength = 0; str[strlength] != '\0'; strlength++)
+		ptstr[strlength] = str[strlength];
+	ptstr[strlength] = '\0';
 	/*return a pointer to the first element of the string */
 	return (ptstr);
 }
