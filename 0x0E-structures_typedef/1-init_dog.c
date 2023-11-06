804146b11282ddc1359eb4dc439fe6dd55d988a7
Susan <susansewe@gmail.com>
2023-11-06 08:24:33 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,9 @@
 #include"dog.h"
 #include<stdlib.h>
+#include<stdio.h>
 /**
- * init_dog - new dog struct
- * Description: whose variables is to be initialized
+ * init_dog - initialize a variable of type
+ * Description: struct dog
  * @d: pointer to the struct
  * @name: name of the dog
  * @age: age of the dog
@@ -11,8 +12,18 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	dog_t mydog;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		printf("Memory allocation failed\n");
+		return;
+	}
+	d = &mydog;
+
 	(*d).name = name;
-	d->age = age;
+	mydog.age = age;
 	d->owner = owner;
 }
 
