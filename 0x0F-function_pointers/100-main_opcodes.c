2856bdcbd83a26f8d1612582835c10c4a9b50100
Susan <susansewe@gmail.com>
2023-11-08 04:20:03 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,51 +1,32 @@
-#include"function_pointers.h"
-#include"3-calc.h"
-#include<stdio.h>
-#include<stdlib.h>
-/**
- * print_opcodes - prints the opcodes(machine language)
- * Description: of the program
- * @a: address
- * @n: number of bytes
- * Return:(void)
- */
-int print_opcodes(void)
-{
-	int i;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n =1)
-		{
-			printf(" ");
-		}
-	}
-	printf("\n")'
+void print_opcodes(char* start, int numBytes)
+{
+	int index;
+
+	for (index = 0; index < numBytes; index++)
+		printf("%02x", (unsigned char)start[index]);
+	printf("\n");
 }
-/**
- * main - prints the opcodes(machine language)
- * Description: of its own main function
- * @argc: number of count
- * @argv: array of input
- * Return:(0)
- */
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	int n;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	n = atoi(argv[1]);
-	if (n < 0)
+	int numBytes = atoi(argv[1]);
+	
+	if (numBytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
-	print_opcodes((char *)&main, n);
+	/* Calculate the address of the start of the main function */
+	char* main_start = (char*)&main;
+	print_opcodes(main_start, numBytes);
+	
 	return (0);
 }
-
