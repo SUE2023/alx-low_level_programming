2f2a406247bd5bfe8ac4cb9eb0272aa5b3901481
Susan <susansewe@gmail.com>
2023-11-21 07:45:23 -0800

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,33 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
+ * find_listint_loop - finds the loop
+ * Description:in a linked list
  * @head: linked list to search for
- *
  * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *second = head;
+	listint_t *first = head;
 
-    if (!head)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
+	while (second && first && first->next)
+	{
+		first = first->next->next;
+		second = second->next;
+		if (first == second)
+		{
+			second = head;
+			while (second != first)
+			{
+				second = second->next;
+				first = first->next;
+			}
+			return (first);
+		}
+	}
+	return (NULL);
 
-    while (slow && fast && fast->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        if (fast == slow)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (fast);
-        }
-    }
-
-    return (NULL);
 }
